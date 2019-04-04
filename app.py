765bca1e7737936b3d08930ecf931c643b917c11
Brian De Maio <bdemaio1@gmail.com>
2019-04-04 15:12:55 -0400

    Setup more CRUD operations for products

--- app.py
+++ app.py
@@ -63,11 +63,43 @@ def add_product():
     return product_schema.jsonify(new_product)
 
 # Get All Products
-@app.route('/product', method=['GET'])
+@app.route('/product', methods=['GET'])
 def get_products():
     all_products = Product.query.all()
     result = products_schema.dump(all_products)
     return jsonify(result.data)
+
+# Get Single Product
+@app.route('/product/<id>', methods=['GET'])
+def get_product(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+# Update a Product
+@app.route('/product/<id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.qty = qty
+
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+# Delete a Product
+@app.route('/product/<id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    return product_schema.jsonify(product)
 
 
 # Run Server
