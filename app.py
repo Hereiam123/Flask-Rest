1117df85de5b403609afec787fa7a1d13074c111
Brian De Maio <bdemaio1@gmail.com>
2019-04-04 14:16:31 -0400

    Setting up Database and Model for saving

--- app.py
+++ app.py
@@ -5,6 +5,34 @@ import os
 
 # Init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'db.sqlite')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init db
+db = SQLAlchemy(app)
+
+# Init marshmallow
+ma = Marshmallow(app)
+
+# Product Class/Model
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = priceself.qty = qty
 
 
 @app.route('/', methods=['GET'])
